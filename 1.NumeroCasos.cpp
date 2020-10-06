c67c72d2faf23f4c528ff8249c359c240e95e1ec
merchf <mercherr@ucm.es>
2020-10-06 13:20:37 +0200

    first commit

---
+++ 1.NumeroCasos.cpp
@@ -0,0 +1,49 @@
+
+// Mercedes Herrero Fernandez
+
+// Comentario general sobre la solución,
+// explicando cómo se resuelve el problema
+
+#include <iostream>
+#include <fstream>
+#include <...>
+
+#include "..."  // propios o los de las estructuras de datos de clase
+
+using namespace std;
+// función que resuelve el problema
+// comentario sobre el coste, O(f(N)), donde N es ...
+Solucion resolver(Datos datos) {
+   ...
+}
+
+// resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+   
+   // leer los datos de la entrada
+   
+   Solucion sol = resolver(datos);
+   
+   // escribir solución
+}
+
+int main() {
+   // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+   std::ifstream in("datos.txt");
+   auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+   
+   int numCasos;
+   std::cin >> numCasos;
+   for (int i = 0; i < numCasos; ++i)
+      resuelveCaso();
+   
+   // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+   std::cin.rdbuf(cinbuf);
+   system("PAUSE");
+#endif
+   return 0;
+}
